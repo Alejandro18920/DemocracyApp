65439efed2abee876c1b05a2d5d900589ba1bef5
Diana Diaz <diana.made@gmail.com>
2022-11-09 17:43:12 -0500

    Modificacion de administracion de resultados y de asignación de partido por referencia a documentos en la base de datos

--- ms_resultados/drivers/driverResultado.py
+++ ms_resultados/drivers/driverResultado.py
@@ -18,35 +18,29 @@ class DriverResultado():
     def index(self):
         return self._repo_resultado.findAll()
 
-###############ASIGNACION CANDIDATO, PARTIDO Y MESA A RESULTADO###############3
-    def create_resultado(self,input,id_mesa, id_partido, id_candidato):
+###############ASIGNACION CANDIDATO Y MESA A RESULTADO###############3
+    def create_resultado(self,input,id_mesa,id_candidato):
         _resultado = Resultado(input)
         elCandidato=Candidato(self._repo_candidato.findById(id_candidato))
         laMesa=Mesa(self._repo_mesa.findById(id_mesa))
-        elPartido=Partido(self._repo_partido.findById(id_partido))
         _resultado.candidato=elCandidato
         _resultado.mesa=laMesa
-        _resultado.partido=elPartido
-
-
         return self._repo_resultado.save(_resultado)
 
     def show(self,id):
         elResultado=Resultado(self._repo_resultado.findById(id))
         return elResultado.__dict__
 
-
-###############MODIFICACION DE RESULTADOS (CANDIDATO, PARTIDO, MESA)################
-    def update_resultado(self,id,input,id_mesa,id_partido,id_candidato):
+    
+###############MODIFICACION DE RESULTADOS (CANDIDATO, MESA)################
+    def update_resultado(self,id,input):
         elResultado=Resultado(self._repo_resultado.findById(id))
-        elResultado.votos=input["votos"]
-        laMesa=Mesa(self._repo_mesa.findById(id_mesa))
-        elCandidato=Candidato(self._repo_candidato.findById(id_candidato))
-        elPartido=Partido(self._repo_partido.findById(id_partido))
-        elResultado.candidato=elCandidato
-        elResultado.mesa=laMesa
-        elResultado.partido=elPartido
+        elResultado.Votos=input["Votos"]
         return self._repo_resultado.save(elResultado)
+
+
+
+###############ELIMINACION DE RESULTADOS################
     
     def delete_resultado(self,id):
         return self._repo_resultado.delete(id)
